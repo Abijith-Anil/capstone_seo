ab9c0a4bd4a31464c5e706f7ca0334aedd82b173
sauravgyawali88 <gyawalisaurav892@gmail.com>
2025-06-10 20:06:36 +0000

    Added console log before and after rendering the App component. Helps confirm app startup and successful mount to DOM during development

--- src/index.js
+++ src/index.js
@@ -3,5 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './App.css';
 
+console.log("App is starting...");
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
+console.log("App has been rendered to the DOM."); 
